af16dc3107b774a36fc3de53863786b6140057a0
Vitalii Rymariev <vitaliirymariev@gmail.com>
2025-07-07 01:05:09 +0300

    try to fix pipeline

--- app/tracks/page.tsx
+++ app/tracks/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { TracksManager } from '@components/tracksManager/TracksManager';
+import { Suspense } from 'react';
 
 export default function Tracks() {
 	return (
 		<div>
 			<main>
-				<TracksManager />
+				<Suspense fallback={<div>Loading...</div>}>
+					<TracksManager />
+				</Suspense>
 			</main>
 		</div>
 	);
