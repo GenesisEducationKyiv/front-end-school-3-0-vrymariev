7b1cf2a90c08bb9b910abf5bf4ce8a21dcac4d73
Vitalii Rymariev <vitaliirymariev@gmail.com>
2025-07-19 13:48:44 +0300

    add fonts example

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-	stories: ['../stories/**/*.mdx', '../components/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	stories: ['../stories/**/*.mdx', '../components/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)', "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-vitest'],
 	framework: {
 		name: '@storybook/nextjs-vite',
--- components/tracksManager/TracksManager.tsx
+++ components/tracksManager/TracksManager.tsx
@@ -6,17 +6,6 @@ import { TrackModalWrapper } from './TrackModalWrapper';
 export const TracksManager: React.FC = () => {
 	return (
 		<div className="max-w-400 mx-auto">
-      <div className="bg-primary text-primary-foreground p-4 rounded">
-        Це primary колір
-      </div>
-      <div className="bg-secondary text-secondary-foreground p-4 rounded">
-        Це secondary колір
-      </div>
-      <div className="bg-accent text-accent-foreground p-4 rounded">
-        Це accent колір
-      </div>
-	  <div className="bg-primary text-primary-foreground p-4">Tailwind кольори</div>
-<div className="bg-[var(--primary)] text-[var(--primary-foreground)] p-4">CSS змінні</div>
 			<h1 className="font-semibold text-4xl mt-10 mb-5 " data-testid="tracks-header">
 				Tracks manager
 			</h1>
